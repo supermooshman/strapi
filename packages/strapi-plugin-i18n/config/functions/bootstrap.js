c09b8610642ba656e417844e1f48a42668d26703
Alexandre Bodin <bodin.alex@gmail.com>
2021-01-04 22:01:37 +0100

    Init i18n fields

--- packages/strapi-plugin-i18n/config/functions/bootstrap.js
+++ packages/strapi-plugin-i18n/config/functions/bootstrap.js
@@ -1,3 +1,36 @@
 'use strict';
 
-module.exports = () => {};
+const _ = require('lodash');
+const { prop } = require('lodash/fp');
+
+module.exports = () => {
+  // register custom permissions
+
+  // register database mixin to modify model behaviours
+
+  // update model lifecycles
+
+  // create the localization of the object & link it to the other localizations it has
+
+  Object.values(strapi.contentTypes).forEach(contentType => {
+    if (prop('pluginOptions.i18n.enabled')(contentType) === true) {
+      console.log('i18N is enabled for ', contentType.modelName);
+
+      const model = strapi.getModel(contentType.uid);
+
+      _.set(model, 'lifecycles.beforeCreate', async () => {});
+
+      _.set(model, 'lifecycles.afterCreate', async () => {});
+
+      _.set(model, 'lifecycles.beforeFind', async () => {});
+    }
+  });
+
+  // wrap content manager routes
+
+  strapi.plugin('content-manager').config.routes.forEach(() => {
+    // add a policy to the route we want to extend
+  });
+
+  // or overwrite controllers
+};
